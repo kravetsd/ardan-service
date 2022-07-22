0160e7b7d2ac25c8e4d4f8a9263c7a42da6f31c3
kravetsd <kdykrg@gmail.com>
2022-07-22 17:59:06 +0300

    added service start and stop with channel

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 var build string = "develop"
 
 func main() {
 	fmt.Println("Hello ardanlabs serivice", build)
+	log.Println("starting ardan-service", "version", build)
+	defer log.Println("shutdown complete")
+
+	// Make a channel to listen for an interrupt or terminate signal from the OS.
+	// Use a buffered channel because the signal package requires it.
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdown
+
+	log.Println("stoping ardan-service")
+
 }
--- makefile
+++ makefile
@@ -5,3 +5,20 @@ run:
 
 build: 
 	go build -ldflags "-X main.build=local"
+
+
+
+# ==============================================================================
+# Building containers
+
+VERSION := 1.0
+
+all: ardan-service
+
+ardan-service:
+	docker build \
+		-f zarf/docker/dockerfile\
+		-t sales-api-amd64:$(VERSION) \
+		--build-arg BUILD_REF=$(VERSION) \
+		--build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+		.
